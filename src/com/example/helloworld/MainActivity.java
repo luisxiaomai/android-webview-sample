7d57f7551d587e3fbababc2027b0f41cb2a5f60b
i072687 <luis.lu@sap.com>
2014-04-13 15:19:06 +0800

    remove tab indent and use 4 spaces as replacement

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -20,68 +20,68 @@ public class MainActivity extends Activity {
     private Button btn;    
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	setContentView(R.layout.activity_main);
-    	webview=(WebView)findViewById(R.id.webView1);
-    	webview.getSettings().setJavaScriptEnabled(true);
-    	edittext=(EditText)findViewById(R.id.editText1);
-    	btn = (Button)findViewById(R.id.button1);
-    	if(webview!=null)
-    	{
-    		webview.setWebViewClient(new MyWebViewClient());
-    		webview.setWebChromeClient(new MyWebChromeClient());
-    	};
-    	btn.setOnClickListener(new View.OnClickListener() {
-    		@Override
-    		public void onClick(View arg0) {
-    			webview.loadUrl(edittext.getText().toString());
-    		}
-    	});
-    	webview.loadUrl("http://www.baidu.com");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview=(WebView)findViewById(R.id.webView1);
+        webview.getSettings().setJavaScriptEnabled(true);
+        edittext=(EditText)findViewById(R.id.editText1);
+        btn = (Button)findViewById(R.id.button1);
+        if(webview!=null)
+        {
+            webview.setWebViewClient(new MyWebViewClient());
+            webview.setWebChromeClient(new MyWebChromeClient());
+        };
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                webview.loadUrl(edittext.getText().toString());
+            }
+        });
+        webview.loadUrl("http://www.baidu.com");
     }
     
     public boolean onKeyDown(int keyCode,KeyEvent event)
     {
-    	if((keyCode==KeyEvent.KEYCODE_BACK)&&webview.canGoBack())
-    	{
-    		webview.goBack();
-    		return true;
-    	}
-    	return super.onKeyDown(keyCode,event);
+        if((keyCode==KeyEvent.KEYCODE_BACK)&&webview.canGoBack())
+        {
+            webview.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
     }
 
     public class MyWebViewClient extends WebViewClient
     {
-    	public boolean shouldOverviewUrlLoading(WebView view,String url)
-    	{
-    		view.loadUrl(url);
-    		return true;
-    	}
-    	@Override
-    	public void onPageFinished(WebView view, String url) {
-    		// TODO Auto-generated method stub
-    		super.onPageFinished(view, url);
-    	}
-    	@Override
-		public void onPageStarted(WebView view, String url, Bitmap favicon) {
-			// TODO Auto-generated method stub
-			super.onPageStarted(view, url, favicon);
-		}    
-	}
+        public boolean shouldOverviewUrlLoading(WebView view,String url)
+        {
+            view.loadUrl(url);
+            return true;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+        }
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }    
+    }
 
-	public class MyWebChromeClient extends WebChromeClient
-	{
-		private ProgressDialog dialog;
-		@Override
-		public void onProgressChanged(WebView view, int newProgress) {
-			if(dialog==null){
-				dialog=ProgressDialog.show(MainActivity.this,null,"正在加载中。。。");
-			}
-			if(newProgress>=100 && dialog!=null){
-				dialog.dismiss();
-				dialog=null;
-			}
-			super.onProgressChanged(view, newProgress);
-		}  
-	}
+    public class MyWebChromeClient extends WebChromeClient
+    {
+        private ProgressDialog dialog;
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            if(dialog==null){
+                dialog=ProgressDialog.show(MainActivity.this,null,"正在加载中。。。");
+            }
+            if(newProgress>=100 && dialog!=null){
+                dialog.dismiss();
+                dialog=null;
+            }
+            super.onProgressChanged(view, newProgress);
+        }
+    }  
 }
